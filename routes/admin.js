684eb5dcc5dc2dc05e59871e52aebc4a64e56080
donald1993-cisse <donald@smartcodegroup.com>
2021-08-06 09:46:12 +0100

    Initial Commit

---
+++ routes/admin.js
@@ -0,0 +1,308 @@
+const express = require('express')
+const router = express.Router()
+const puppeteer = require('puppeteer')
+const cheerio = require('cheerio')
+
+//requiring product model
+let Product = require('../models/product')
+
+//checks if the user is authenticated
+function isAuthenticatedUser(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next()
+    }
+    req.flash('error', 'Please login first to access this page.')
+    res.redirect('/login')
+}
+
+
+let browser
+
+
+//Scrappe function
+async function scrapeData(url, page) {
+    try {
+        await page.goto(url, {waitUntil : 'load', timeout : 0})
+        const html = await page.evaluate(() => document.body.innerHTML)
+        const $ = await cheerio.load(html)
+
+        let title = $("h1").attr("content")
+        let price = $(".price-characteristic").attr("content")
+
+        if(!price) {
+            let dollars = $("#price > div > span.hide-content.display-inline-block-m > span > span.price-group > span.price-characteristic").text()
+
+            let cents = $("#price > div > span.hide-content.display-inline-block-m > span > span.price-group > span.price-mantissa").text()
+
+            price = dollars+'.'+cents
+        }
+
+        let seller = ''
+        let checkSeller = $(".seller-name")
+        if(checkSeller) {
+            seller = checkSeller.text()
+        }
+
+        let outOfStock = ''
+        let checkOutOfStock = $('.prod-ProductOffer-oosMsg')
+        if(checkOutOfStock) {
+            outOfStock = checkOutOfStock.text()
+        }
+
+        let deliveryNotAvailable = ''
+        let checkDeliveryNotAvailable = $(".fulfillment-shipping-text")
+        if(checkDeliveryNotAvailable) {
+            deliveryNotAvailable = checkDeliveryNotAvailable.text()
+        }
+
+        let stock = ''
+
+        if(!(seller.includes('Walmart')) || outOfStock.includes('Out Of Stock') || deliveryNotAvailable.includes('Delivery not available')) {
+            stock = 'Out of Stock'
+        } else {
+            stock = 'In stock'
+        }
+
+        return {
+            title,
+            price,
+            stock,
+            url
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+//GET routes starts here
+
+router.get('/', (req, res) => {
+    res.render('./admin/index')
+})
+
+router.get('/dashboard', isAuthenticatedUser, (req, res) => {
+    Product.find({})
+    .then(product => {
+        res.render('./admin/dashboard', {product: product})
+    })
+    
+})
+
+router.get('/product/new', isAuthenticatedUser, async (req, res) => {
+    try {
+        let url = req.query.search
+        if(url) {
+            browser = await puppeteer.launch({headless:false})
+            const page = await browser.newPage()
+            let result = await scrapeData(url, page)
+
+            let productData = {
+                title : result.title,
+                price : '$'+result.price,
+                stock : result.stock,
+                productUrl : result.url
+            }
+
+            res.render('./admin/newproduct', {productData : productData})
+            browser.close()
+        }
+        else {
+            let productData = {
+                title : "",
+                price : "",
+                stock : "",
+                productUrl : ""
+            }
+            res.render('./admin/newproduct', {productData : productData})
+        }
+    } catch (error) {
+        req.flash('error', 'ERROR: ' + error)
+        res.redirect('/product/new')
+    }
+})
+
+
+router.get('/product/search', isAuthenticatedUser, (req, res) => {
+    let userSku = req.query.sku
+    if(userSku) {
+        Product.findOne({sku : userSku})
+            .then(product => {
+                if(!product) {
+                    req.flash('error', 'Product does not exist.')
+                    return res.redirect('/product/search')
+                }
+
+                res.render('./admin/search', {productData : product})
+            })
+            .catch(err => {
+                req.flash('error', 'ERROR: ' + err)
+                res.redirect('/product/new')
+            })
+    } else {
+        res.render('./admin/search', {productData : ''})
+    }
+})
+
+router.get('/product/instock', isAuthenticatedUser, (req, res) => {
+    Product.find({newstock : "In stock"})
+        .then(product => {
+            res.render('./admin/instock', {product : product})
+        })
+        .catch(err => {
+            req.flash('error', 'ERROR: ' + err)
+            res.redirect('/dashboard')
+        })
+})
+
+router.get('/product/outofstock', isAuthenticatedUser, (req, res) => {
+    Product.find({newstock : "Out of Stock"})
+        .then(product => {
+            res.render('./admin/outofstock', {product : product})
+        })
+        .catch(err => {
+            req.flash('error', 'ERROR: ' + err)
+            res.redirect('/dashboard')
+        })
+})
+
+router.get('/product/pricechanged', isAuthenticatedUser, (req, res) => {
+    Product.find({})
+        .then(product => {
+            res.render('./admin/pricechanged', {product : product})
+        })
+        .catch(err => {
+            req.flash('error', 'ERROR: ' + err)
+            res.redirect('/dashboard')
+        })
+})
+
+router.get('/product/backinstock', isAuthenticatedUser, (req, res) => {
+    Product.find({$and: [{oldstock: "Out of Stock"}, {newstock: "In stock"}]})
+        .then(product => {
+            res.render('./admin/backinstock', {product : product})
+        })
+        .catch(err => {
+            req.flash('error', 'ERROR: ' + err)
+            res.redirect('/dashboard')
+        })
+})
+
+router.get('/product/updated', isAuthenticatedUser, (req, res) => {
+    Product.find({updatestatus : "Updated"})
+        .then(product => {
+            res.render('./admin/updatedproducts', {product : product})
+        })
+        .catch(err => {
+            req.flash('error', 'ERROR: ' + err)
+            res.redirect('/dashboard')
+        })
+})
+
+router.get('/product/notupdated', isAuthenticatedUser, (req, res) => {
+    Product.find({updatestatus : "Not Updated"})
+        .then(product => {
+            res.render('./admin/notupdatedproducts', {product : product})
+        })
+        .catch(err => {
+            req.flash('error', 'ERROR: ' + err)
+            res.redirect('/dashboard')
+        })
+})
+
+router.get('/update', isAuthenticatedUser, (req, res) => {
+    res.render('./admin/update', {message: ''})
+})
+
+
+//POST routes starts here
+
+router.post('/product/new', isAuthenticatedUser, (req, res) => {
+    let {title, price, stock, url, sku} = req.body
+
+    let newProduct = {
+        title : title,
+        newprice : price,
+        oldprice : price,
+        oldstock : stock,
+        newstock : stock,
+        sku : sku,
+        company : "Walmart",
+        url : url,
+        updatestatus : "Updated"
+    }
+
+    Product.findOne({sku : sku})
+        .then(product => {
+            if(product) {
+                req.flash('error', 'Product already exist in the database.')
+                return res.redirect('/product/new')
+            }
+
+            Product.create(newProduct)
+                .then(product => {
+                    req.flash('success_msg', 'Product saved successfully.')
+                    res.redirect('/product/new')
+                })
+        })
+        .catch(err => {
+            req.flash('error', 'ERROR: ' + err)
+            res.redirect('/product/new')
+        })
+})
+
+router.post('/update', isAuthenticatedUser, async(req, res) => {
+    try {
+        res.render('./admin/update', {message : 'update started'})
+
+        Product.find({})
+            .then(async products => {
+                for(let i=0; i<products.length; i++) {
+                    Product.updateOne({'url': products[i].url}, {$set: {'oldprice': products[i].newprice, 'oldstock': products[i].newstock, 'updatestatus': 'Not Updated'}})
+                    .then(products => {})
+                }
+
+                browser = await puppeteer.launch({headless: false})
+                const page = await browser.newPage()
+
+                for(let i=0; i<products.length; i++) {
+                    let result = await scrapeData(products[i].url, page)
+                    Product.updateOne({'url': products[i].url}, {$set: {'title': result.title, 'newprice': '$'+result.price, 'newstock': result.stock, 'updatestatus': 'Updated'}})
+                        .then(products => {})
+                }
+
+                browser.close()
+            })
+            .catch(err => {
+                req.flash('error', 'ERROR: ' + error)
+                res.redirect('/dashboard')
+            })
+    } catch (error) {
+        req.flash('error', 'ERROR: ' + error)
+        res.redirect('/dashboard')
+    }
+})
+
+//DELETE route
+router.delete('/delete/product/:id', isAuthenticatedUser, (req, res) => {
+    let searchQuery = {_id: req.params.id}
+
+    Product.deleteOne(searchQuery)
+        .then(product => {
+            req.flash('success_msg', 'Product deleted successfully.')
+            res.redirect('/dashboard')
+        })
+        .catch(err => {
+            req.flash('error', 'ERROR: ' + err)
+            res.redirect('/dashboard')
+        })
+})
+
+router.get('*', (req, res) => {
+    res.render('./admin/notfound')
+})
+
+
+
+module.exports = router
---
+++ routes/users.js
@@ -0,0 +1,287 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const crypto = require('crypto')
+const async = require('async')
+const nodemailer = require('nodemailer')
+
+//requiring use model
+const User = require('../models/usermodel')
+
+//checks if the user is authenticated
+function isAuthenticatedUser(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next()
+    }
+    req.flash('error', 'Please login first to access this page.')
+    res.redirect('/login')
+}
+ 
+//GET routes
+router.get('/login', (req, res) => {
+    res.render('./users/login')
+})
+
+router.get('/signup', isAuthenticatedUser, (req, res) => {
+    res.render('./users/signup')
+})
+
+router.get('/logout', isAuthenticatedUser, (req, res) => {
+    req.logOut()
+    req.flash('success_msg', 'You have been logged out.')
+    res.redirect('/login')
+})
+
+router.get('/forgot', (req, res) => {
+    res.render('./users/forgot')
+})
+
+router.get('/reset/:token', (req, res) => {
+    User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}})
+        .then(user => {
+            if(!user) {
+                req.flash('error', 'Password reset token is invalid or has expired.')
+                res.redirect('/forgot')
+            }
+            res.render('./users/newpassword', {token: req.params.token})
+        })
+        .catch(err => {
+            req.flash('error', 'ERROR: ' + err)
+            res.redirect('/forgot')
+        })
+})
+
+router.get('/password/change', isAuthenticatedUser, (req, res) => {
+    res.render('./users/changepassword')
+})
+
+router.get('/users/all', isAuthenticatedUser, (req, res) => {
+    User.find({})
+        .then(users => {
+            res.render('./users/allusers', {users : users})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+router.get('/edit/:id', isAuthenticatedUser, (req, res) => {
+    let searchQuery = {_id : req.params.id}
+
+    User.findOne(searchQuery)
+        .then(user => {
+            res.render('./users/edituser', {user : user})
+        })
+        .catch(err => {
+            req.flash('error', 'ERROR: ' + err)
+            res.redirect('/users/all')
+    })
+})
+
+
+//POST routes
+router.post('/login', passport.authenticate('local', {
+    successRedirect : '/dashboard',
+    failureRedirect : '/login',
+    failureFlash : 'Invalid email or password. Try again!!!'
+}))
+
+router.post('/signup', isAuthenticatedUser, (req, res) => {
+    let {name, email, password} = req.body
+
+    let userData = {
+        name : name,
+        email : email
+    }
+
+    User.register(userData, password, (err, user) => {
+        if(err) {
+            req.flash('error_msg', 'ERROR: ' + err)
+            res.redirect('/signup')
+        }
+        req.flash('success_msg', 'Account created successfully')
+        res.redirect('/signup')
+    })
+})
+
+router.post('/password/change', (req, res) => {
+    if(req.body.password !== req.body.confirmpassword) {
+        req.flash('error', "Password don't match. Try again!!!")
+        return res.redirect('/password/change')
+    }
+
+    User.findOne({email: req.user.email})
+        .then(user => {
+            user.setPassword(req.body.password, err => {
+                user.save()
+                    .then(user => {
+                        req.flash('success_msg', 'Password changed successfully.')
+                        res.redirect('/password/change')
+                    })
+                    .catch(err => {
+                        req.flash('error', 'ERROR: ' + err)
+                        res.redirect('/password/change')
+                    })
+            })
+        })
+})
+
+
+//route to handle forgot password
+router.post('/forgot', (req, res, next) => {
+    let recoveryPassword = ''
+    async.waterfall([
+        (done) => {
+            crypto.randomBytes(20, (err, buf) => {
+                let token = buf.toString('hex')
+                done(err, token)
+            })
+        },
+        (token, done) => {
+            User.findOne({email: req.body.email})
+                .then(user => {
+                    if(!user) {
+                        req.flash('error', 'User does not exist with this email.')
+                        return res.redirect('/forgot')
+                    }
+
+                    user.resetPasswordToken = token
+                    user.resetPasswordExpires = Date.now() + 1800000   //   30 minutes
+
+                    user.save(err => {
+                        done(err, token, user)
+                    })
+                })
+                .catch(err => {
+                    req.flash('error', 'ERROR: ' + err)
+                    res.redirect('/forgot')
+                })
+        },
+        (token, user) => {
+            let smtpTransport = nodemailer.createTransport({
+                service : 'Gmail',
+                auth : {
+                    user : process.env.GMAIL_EMAIL,
+                    pass : process.env.GMAIL_PASSWORD
+                }
+            })
+
+            let mailOptions = {
+                to: user.email,
+                from: 'Ndeloko Ulrich ulrichflynn9@gmail.com',
+                subject: 'Recovery email from Authentication App',
+                text: 'Please click the following link to recover your password: \n\n' +
+                      'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                      'If you did not request this, please ignore this mail.'
+            }
+            smtpTransport.sendMail(mailOptions, err => {
+                req.flash('success_msg', 'An email has been sent to this address. Please check your email.')
+                res.redirect('/forgot')
+            })
+        }
+
+    ], err => {
+        if(err) res.redirect('/forgot')
+    })
+})
+
+
+router.post('/reset/:token', (req, res) => {
+    async.waterfall([
+        (done) => {
+            User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}})
+            .then(user => {
+                if(!user) {
+                    req.flash('error', 'Password reset token is invalid or has expired.')
+                    res.redirect('/forgot')
+                }
+
+                if(req.body.password !== req.body.confirmpassword) {
+                    req.flash('error', "Password don't match.")
+                    return res.redirect('/forgot')
+                }
+
+                user.setPassword(req.body.password, err => {
+                    user.resetPasswordToken = undefined
+                    user.resetPasswordExpires = undefined
+
+                    user.save(err => {
+                        req.logIn(user, err => {
+                            done(err, user)
+                        })
+                    })
+                })
+            })
+            .catch(err => {
+                req.flash('error', 'ERROR: ' + err)
+                res.redirect('/forgot')
+            })
+        },
+        (user) => {
+            let smtpTransport = nodemailer.createTransport({
+                service : 'Gmail',
+                auth : {
+                    user : process.env.GMAIL_EMAIL,
+                    pass : process.env.GMAIL_PASSWORD
+                }
+            })
+
+            let mailOptions = {
+                to: user.email,
+                from: 'Ndeloko Ulrich ulrichflynn9@gmail.com',
+                subject: 'Your password has been changed.',
+                text: 'Hello, ' + user.name + '.\n\n' +
+                      'This is the confirmation that the password for your account ' +user.email+ ' has been changed.'
+            }
+
+            smtpTransport.sendMail(mailOptions, err => {
+                req.flash('success_msg', 'Your password has been changed successfully.')
+                res.redirect('/login')
+            })
+        }
+
+    ], err => {
+        res.redirect('/login')
+    })
+})
+
+
+//PUT routes starts here
+router.put('/edit/:id', (req, res) => {
+    let searchQuery = {_id : req.params.id}
+
+    User.updateOne(searchQuery, {$set : {
+        name : req.body.name,
+        email : req.body.email
+        }
+    })
+    .then(user => {
+        req.flash('success_msg', 'User updated successfully.')
+        res.redirect('/users/all')
+    })
+    .catch(err => {
+        req.flash('error', 'ERROR: ' + err)
+        res.redirect('/users/all')
+    })
+})
+
+
+//Delete route starts here
+router.delete('/delete/user/:id', (req, res) => {
+    let searchQuery = {_id : req.params.id}
+
+    User.deleteOne(searchQuery)
+        .then(user => {
+            req.flash('success_msg', 'User deleted successfully.')
+            res.redirect('/users/all')
+        })
+        .catch(err => {
+            req.flash('error', 'ERROR: ' + err)
+            res.redirect('/users/all')
+        })
+})
+
+
+
+
+module.exports = router
